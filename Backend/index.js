2a5ea2e71f9f599661edbe4eeb7c952d500b3d18
SchrodingerrCat <amazingishika2@gmail.com>
2024-10-13 20:55:56 +0530

    App Completed

--- Backend/index.js
+++ Backend/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv" ;
 import mongoose from "mongoose";
 import cors from "cors" ;
 import cookieParser from "cookie-parser" ;
+import  path  from "path";
 
 import userRoute from "./routes/user.route.js";
 import messageRoute from "./routes/message.route.js";
@@ -26,9 +27,20 @@ try{
     console.log(error) ;
 }
 
+
 //routes
 app.use("/api/user",userRoute)
 app.use("/api/message",messageRoute)
+
+// ********** code for deployement ***********
+if(process.env.NODE_ENV  === "production"){
+  const  dirPath = path.resolve() ;
+
+  app.use(express.static("./Frontend/dist")) ;
+  app.get("*",(req,res)=>{
+    res.sendFile(path.resolve(dirPath, "./Frontend/dist", "index.html"));
+  })
+}
 
 server.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`)
